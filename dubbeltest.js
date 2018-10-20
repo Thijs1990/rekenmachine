bed2712f98023572cf41eac000203a36e5ba9025
fabblwaps <44229767+fabblwaps@users.noreply.github.com>
2018-10-20 15:56:29 +0200

    Update dubbeltest.js

--- dubbeltest.js
+++ dubbeltest.js
@@ -11,28 +11,57 @@ var resOperator = [];
 
 document.onclick = function(event) {
 	if (event.target.className == 'number') {
-		tmpGetal += (event.target.value)
-		scherm.innerText = tmpGetal
+		tmpGetal += (event.target.value);
+		scherm.innerText = tmpGetal;
 	}
 	else if ((event.target.className == 'decimal') && (tmpGetal.indexOf(".") == -1)) {
-		tmpGetal += (event.target.value)
-		scherm.innerText = tmpGetal
+		tmpGetal += (event.target.value);
+		scherm.innerText = tmpGetal;
 
 	}
-	else if (event.target.className == 'operator') {
+	else if (event.target.className == 'operator' && resOperator.length == 0) {
 		resDecimal.push(tmpGetal);
 		tmpGetal = "";
 		resOperator.push(event.target.value);
 		scherm.innerText = event.target.value;
+		//function(uitrekenen);
 	}
 	else if (event.target.className == 'clear') {
-		tmpGetal = "";
-		resDecimal = "";
-		resOperator = "";
-		scherm.innerText = tmpGetal
-	}
+		clear()
+		scherm.innerText = tmpGetal;	
+		}
 	else if (event.target.className == 'calculate') {
-		scherm.innerText = ("calculate")
+		resDecimal.push(tmpGetal);
+		scherm.innerText = uitrekenen(resDecimal[0], resDecimal[1], resOperator[0]);
+		clear();
+	};
+}
+function clear(){
+		tmpGetal = "";
+		resDecimal = [];
+		resOperator = [];
+	}
+		
+function uitrekenen(getal1, getal2, operator) {
+	console.log(getal1);
+	console.log(operator);
+	console.log(getal2);
+	if (operator == "+") {
+		return(parseFloat(getal1) + parseFloat(getal2))
+	}
+	else if (operator == "-") {
+		return(parseFloat(getal1) - parseFloat(getal2))
+	}
+	else if (operator == "/") {
+		if (parseFloat(getal2) == 0) {
+			return("ERROR")
+		}
+		else {
+			return(parseFloat(getal1) / parseFloat(getal2))
+		};
+	}
+	else if (operator == "X") {
+		return(parseFloat(getal1) * parseFloat(getal2))
 	};
 }
 
